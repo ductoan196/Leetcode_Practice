cf0887fed31059d9b333cb1b8bbce881bb0400ad
ductoan196 <ductoannd96@gmail.com>
2023-02-02 17:07:56 +0700

    update

--- Day1/src/main/java/LC27_RemoveElement.java
+++ Day1/src/main/java/LC27_RemoveElement.java
@@ -5,8 +5,16 @@ import java.util.List;
 public class LC27_RemoveElement {
     public int removeElement(int[] nums, int val) {
         int[] list = new int[nums.length];
+        int count = 0;
         for(int num: nums){
-
+            if(num != val){
+                count +=1;
+                list[count-1] = num;
+            }
         }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = list[i];
+        }
+        return count;
     }
 }
