86a413a9076011e2a1dd80ef1cc00bd34ddd706c
ductoan196 <ductoannd96@gmail.com>
2023-02-07 08:11:47 +0700

    update

--- Day2/src/main/java/LC1662.java
+++ Day2/src/main/java/LC1662.java
@@ -1,13 +1,31 @@
 public class LC1662 {
+//    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//        String s1 = "";
+//        String s2 = "";
+//        for (int i = 0; i < word1.length; i++) {
+//            s1 += word1[i];
+//        }
+//        for (int i = 0; i < word2.length; i++) {
+//            s2 += word2[i];
+//        }
+//        return s1.equals(s2);
+//    }
+//    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//        StringBuilder s1 = new StringBuilder();
+//        StringBuilder s2 = new StringBuilder();
+//
+//        for (int i = 0; i < word1.length; i++) {
+//            s1.append(word1[i]);
+//        }
+//        for (int i = 0; i < word2.length; i++) {
+//            s2.append(word2[i]);
+//        }
+//        return s1.toString().equals(s2.toString());
+//    }
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String s1 = "";
-        String s2 = "";
-        for (int i = 0; i < word1.length; i++) {
-            s1 += word1[i];
-        }
-        for (int i = 0; i < word2.length; i++) {
-            s2 += word2[i];
-        }
+        String s1= String.join("",word1);
+        String s2 = String.join("", word2);
+
         return s1.equals(s2);
     }
 }
--- Day2/src/main/java/LC1816_TrunscateSentence.java
+++ Day2/src/main/java/LC1816_TrunscateSentence.java
@@ -1,10 +1,20 @@
 public class LC1816_TrunscateSentence {
+//    public String truncateSentence(String s, int k) {
+//        String[] newS = s.split(" ");
+//        String sentence = "";
+//        for (int i = 0; i < k; i++) {
+//            sentence += newS[i] + " ";
+//        }
+//        return sentence.trim();
+//    }
     public String truncateSentence(String s, int k) {
         String[] newS = s.split(" ");
-        String sentence = "";
-        for (int i = 0; i < k; i++) {
-            sentence += newS[i] + " ";
+        StringBuilder sentence = new StringBuilder();
+        for (int i = 0; i < k-1; i++) {
+            sentence.append(newS[i]).append(" ");
         }
-        return sentence.trim();
+        sentence.append(newS[k-1]);
+        return sentence.toString();
     }
+
 }
