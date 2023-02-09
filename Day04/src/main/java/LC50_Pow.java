080796c4b042be5ded07512869a2d3d00754fa26
ductoan196 <ductoannd96@gmail.com>
2023-02-09 17:40:47 +0700

    update

--- Day04/src/main/java/LC50_Pow.java
+++ Day04/src/main/java/LC50_Pow.java
@@ -1,12 +1,19 @@
 public class LC50_Pow {
     public static double myPow(double x, int n) {
+        int n_2 = 0;
         if (n==0){
             return 1;
-        } else if (n<0){
-            return 1/(x*myPow(x,-(n+1)));
         }
-        return x*myPow(x,n-1);
+        if (n<0){
+            n_2 = n%2 ==0? -n/2: (-n-1)/2;
+        } else{
+            n_2 = n%2 ==0? n/2: (n-1)/2;
+        }
+
+        double t = myPow(x, n_2);
+        return n%2==0? t*t : x*t*t;
     }
+
 
     public static void main(String[] args) {
         System.out.println(myPow(2,-3));
