a8873c820f22ddc290445ce5e3fba2e6860a8ab3
ductoan196 <ductoannd96@gmail.com>
2023-02-14 21:57:57 +0700

    update

--- Day7_TEST/src/main/java/LC832_FlippingImage.java
+++ Day7_TEST/src/main/java/LC832_FlippingImage.java
@@ -1,2 +1,22 @@
 public class LC832_FlippingImage {
+    //Tạo ra một mảng mới có số hàng, cột bằng với image
+    //chạy vòng for cho từng hàng ở trong mảng
+    //trong mỗi hàng thực hiện đảo ngược vị trí các phần tử 
+
+        public int[][] flipAndInvertImage(int[][] image) {
+            int row = image.length, col = image[0].length;
+            int rs[][] = new int[row][col];
+            for(int i = 0; i < row; i++)
+                rs[i] = reverse(image[i]);
+            return rs;
+        }
+        public int[] reverse(int a[]){
+            int rs[] = new int[a.length]; //tạo một mảng mới hứng giá trị đảo ngược
+            int n = a.length;
+            for(int i = 0; i < n; i++)
+                rs[i] = a[n - 1 - i] == 0 ? 1 : 0;
+            //Toán tử 3 ngôi: nếu tại vị trí được đảo ngược mà =0 thì rs tại đó =1, và còn lại thì =0
+            return rs;
+        }
+
 }
