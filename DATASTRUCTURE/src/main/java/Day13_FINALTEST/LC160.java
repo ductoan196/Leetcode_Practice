08066e4bda9a8fc4ca066939abc7070b5fb274b6
ductoan196 <ductoannd96@gmail.com>
2023-03-07 21:35:07 +0700

    update

--- DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
+++ DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
@@ -1,17 +1,26 @@
 package Day13_FINALTEST;
 
 public class LC160 {
+    //Bài toán yêu cầu tìm node giao nhau của hai linked list.
+    // Độ phức tạp thuật toán của bài toán này là O(m + n)
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
+        ListNode p = headA;
+        ListNode q = headB;
+        while (p != q) {
+            p = p == null ? headB : p.next;
+            q = q == null ? headA : q.next;
+        }
+        return p;
     }
 
 
     public class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) {
-          val = x;
-          next = null;
-      }
-  }
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }
--- DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC1926.java
+++
@@ -1,7 +0,0 @@
-package Day13_FINALTEST;
-
-public class LC1926 {
-    public int nearestExit(char[][] maze, int[] entrance) {
-
-    }
-}
