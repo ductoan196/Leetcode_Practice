469f0269bfebc24a4bc52afebf9c48755f99a650
ductoan196 <ductoannd96@gmail.com>
2023-03-07 21:42:46 +0700

    update

--- DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
+++ DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
@@ -4,9 +4,9 @@ public class LC160 {
     //Bài toán yêu cầu tìm node giao nhau của hai linked list.
     // Độ phức tạp thuật toán của bài toán này là O(m + n)
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode p = headA;
+        ListNode p = headA; //Sử dụng 2 con trỏ p, q cho 2 list
         ListNode q = headB;
-        while (p != q) {
+        while (p != q) { //run đến khi p =q, nếu không có thì sẽ trả về null
             if (p == null) {
                 p = headB;
             } else {
