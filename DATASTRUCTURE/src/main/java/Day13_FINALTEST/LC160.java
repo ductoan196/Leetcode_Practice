a9039c8cd55337d780008e166920ff4c492e9b08
ductoan196 <ductoannd96@gmail.com>
2023-03-07 21:36:40 +0700

    update

--- DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
+++ DATASTRUCTURE/src/main/java/Day13_FINALTEST/LC160.java
@@ -7,8 +7,16 @@ public class LC160 {
         ListNode p = headA;
         ListNode q = headB;
         while (p != q) {
-            p = p == null ? headB : p.next;
-            q = q == null ? headA : q.next;
+            if (p == null) {
+                p = headB;
+            } else {
+                p = p.next;
+            }
+            if (q == null) {
+                q = headA;
+            } else {
+                q = q.next;
+            }
         }
         return p;
     }
